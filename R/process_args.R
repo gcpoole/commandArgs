015a0ffde1e40c9ff5337a8559e54c58d8fd9f04
gcpoole <gpoole@eco-metrics.com>
2024-10-07 11:53:21 -0600

    Correct a couple of bugs and update documentation

--- R/process_args.R
+++ R/process_args.R
@@ -24,11 +24,13 @@
 #'   parameter name was absent, or the value after the `=` if assigned a value
 #'   on in `args`.
 #'
-#'   If `args` contains a value `--verbose` the return vector is printed
-#'   to the console unless `quietly` is set to TRUE.
+#'   If `args` contains a value `--verbose` the return vector is printed to the
+#'   console unless `quietly` is set to TRUE.
 #'
 #' @param args Character vector in form of `--<parameter>`  or
-#'   `--<parameter>=<value>`, usually derived by calling commandArgs()
+#'   `--<parameter>=<value>`, usually derived using `commandArgs()[-1]` (note
+#'   that the first value returned from commandArgs() is the location of the R
+#'   executable, so it is removed from `args` using `[-1]`)
 #' @param required_value_args A character vector of parameter names that are
 #'   required on the command line and which must be assigned arguments values.
 #' @param required_flag_args A character vector of parameter names that are
@@ -42,24 +44,19 @@
 #' @param ignored_args A character vector of parameter names that will be
 #'   removed from the return value.
 #' @param quietly Set to TRUE to suppress printing of arguments to console even
-#' if `args` contains '--verbose'
+#'   if `args` contains '--verbose'
 #'
 #' @export
-process_args <- function(args, required_args,
-                         optional_value_args, optional_flag_args,
+process_args <- function(args, required_value_args = character(0), required_flag_args = character(0),
+                         optional_value_args = character(0), optional_flag_args = character(0),
                          ignored_args = c("no-echo", "no-restore", "file", "args"),
                          quietly = FALSE)
 {
 
-  required_args <- c("source-file", "output-file", "title-id", "version-id")
-  # these args must have a value after '='
-  optional_value_args <-
-    c("characters", "genres", "point-of-view", "language", "assets")
-  # these args may have a value but don't need one
-  optional_flag_args <-
-    c("throw-error", "throw-warnings", "sample-data", "verbose", "scanned-book")
-  ignored_args <- c("no-echo", "no-restore", "file", "args")
+  required_args <- c(required_value_args, required_flag_args)
   optional_args <- c(optional_value_args, optional_flag_args)
+  need_value_args <- c(required_value_args, optional_value_args)
+  valid_args <- c(required_args, optional_args, ignored_args)
 
   # check that all args are formatted correctly
   badArgs = !grepl("(^--[^[:space:]]+=[^[:space:]]+.+$)|(^--[^[:space:]]+$)", args)
@@ -80,14 +77,14 @@ process_args <- function(args, required_args,
         "The following required command line arguments are missing: ",
         paste0(missing_args, collapse = ", ")))
 
-  invalid_args <- names(args)[!names(args) %in% c(required_args, optional_args, ignored_args)]
+  invalid_args <- names(args)[!names(args) %in% valid_args]
   if(length(invalid_args) > 0 )
     stop("The following command line arguments are not recognized: ",
          paste0(invalid_args, collapse = ", "))
 
   # If optional and ignored args are NA, force to "TRUE"; missing, force to
   # "FALSE" (remember, args are of type character...)
-  for(arg_name in c(ignored_args, optional_args)) {
+  for(arg_name in valid_args) {
     if(arg_name %in% names(args)) {
       if(is.na(args[arg_name])) args[arg_name] = "TRUE"
     } else {
@@ -97,12 +94,12 @@ process_args <- function(args, required_args,
 
   badArgs <-
     sapply(
-      c(required_value_args, optional_value_args),
+     need_value_args,
       function(x) args[x] == "TRUE", USE.NAMES = F)
 
   if(any(badArgs))
     stop("When present, the following command line args must have values: '",
-         paste(optional_value_args[badArgs], collapse = "', '"), "'.")
+         paste(need_value_args[badArgs], collapse = "', '"), "'.")
 
   args <- args[!names(args) %in% ignored_args]
 
